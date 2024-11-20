32f6425a9b10f5ba16329f5644905b1f43fa64b6
9090418 <99524707+9090418@users.noreply.github.com>
2024-11-21 00:38:57 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/meituanredirect/MainActivity.java
+++ app/src/main/java/com/example/meituanredirect/MainActivity.java
@@ -6,22 +6,16 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        // 跳转到指定的 URL
+        Uri uri = Uri.parse("imeituan://www.meituan.com/web?url=https://awp.meituan.com/payfe/union-pay-qrcode/index.html");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
 
-        // Redirect to the Meituan URL
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setData(Uri.parse("imeituan://www.meituan.com/web?url=https://awp.meituan.com/payfe/union-pay-qrcode/index.html"));
-
-        try {
-            startActivity(intent);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        // Close the activity after the redirect
+        // 关闭当前的 MainActivity
         finish();
     }
 }
